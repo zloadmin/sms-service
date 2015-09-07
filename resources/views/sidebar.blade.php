5e9e2e4a01edbd2a76f668859221a206f373eedc
zloadmin <ded@xakep.ru>
2015-09-08 03:18:17 +0500

    menu active

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,12 +1,25 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li><a href="/smslist/create">Новая рассылка</a></li>
-        <li><a href="/smslist/list">Списки рассылки ({{ Auth::user()->smslist()->notdraft()->count() }})</a></li>
-        <li><a href="/smslist/list_draft">Черновики рассылки ({{ Auth::user()->smslist()->draft()->count() }})</a></li>
+
+        <li class={!! Request::path() == 'smslist/create' ? 'active' : ''; !!}>
+            <a href="/smslist/create">Новая рассылка</a>
+        </li>
+        <li class={!! Request::path() == 'smslist/list' ? 'active' : ''; !!}>
+            <a href="/smslist/list">Списки рассылки ({{ Auth::user()->smslist()->notdraft()->count() }})</a>
+        </li>
+        <li class={!! Request::path() == 'smslist/list_draft' ? 'active' : ''; !!}>
+            <a href="/smslist/list_draft">Черновики рассылки ({{ Auth::user()->smslist()->draft()->count() }})</a>
+        </li>
     </ul>
     <ul class="nav nav-sidebar">
-        <li><a href="/number_group/create">Добавить список</a></li>
-        <li><a href="/number_group/list">Мои абоненты ({{ Auth::user()->numbersgroup()->count() }})</a></li>
-        <li><a href="/number_group/system_list">Абоненты сервиса ({{ App\NumbersGroup::syslist()->count() }})</a></li>
+        <li class={!! Request::path() == 'number_group/create' ? 'active' : ''; !!}>
+            <a href="/number_group/create">Добавить список</a>
+        </li>
+        <li class={!! Request::path() == 'number_group/list' ? 'active' : ''; !!}>
+            <a href="/number_group/list">Мои абоненты ({{ Auth::user()->numbersgroup()->count() }})</a>
+        </li>
+        <li class={!! Request::path() == 'number_group/system_list' ? 'active' : ''; !!}>
+            <a href="/number_group/system_list">Абоненты сервиса ({{ App\NumbersGroup::syslist()->count() }})</a>
+        </li>
     </ul>
 </div>
